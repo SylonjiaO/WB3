a109ab7cd36225528720629cffe47331be5fe5fa
Sylonjia Odion <Sodion@atl.yearup.org>
2022-10-03 11:47:52 -0400

    functions added

---
+++ functions/simplefunctions.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function favoriteThings() {
+    console.log("Sylonjia Odion");
+    console.log("Scary Dinosaurs") 
+   console.log("joji")
+    
+    
+}
+
+
+function whyImHere() {
+    console.log("To prove to myself that i can");
+    console.log("To code fulltime"); 
+    
+    
+}
+
+
+function favoritePlace() {
+    console.log("home");
+    
+    
+}
+
+favoriteThings();
+favoritePlace();
+whyImHere();
