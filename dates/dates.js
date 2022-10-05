1c90e67f79926c598d8fc2a9cf117a3830c46d1c
Sylonjia Odion <Sodion@atl.yearup.org>
2022-10-05 09:51:41 -0400

    dates

---
+++ dates/dates.js
@@ -0,0 +1,5 @@
+let date = new Date();
+console.log(date.toString()); //toString
+console.log (date.toDateString()); //toDateString
+console.log (date.toLocaleString()); //toLocaleString
+console.log (date.toUTCString()); //toUTCString
